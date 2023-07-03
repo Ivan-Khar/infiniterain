ec9dcaa7a32f4b18a537fe6c443800274811f94c
Ivan Kh <47220198+Ivan-Khar@users.noreply.github.com>
2023-07-03 16:52:11 +0300

    Fix for 1.16.5 version

--- src/main/java/com/aqupd/infiniterain/InfiniteRain.java
+++ src/main/java/com/aqupd/infiniterain/InfiniteRain.java
@@ -6,15 +6,10 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Objects;
 
 @Mod(InfiniteRain.MODID)
 public class InfiniteRain {
     public static final String MODID = "infiniterain";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
     private long time = 0L;
 
@@ -24,12 +19,14 @@ public class InfiniteRain {
 
     @SubscribeEvent
     public void onServerTick(TickEvent.WorldTickEvent event) {
-        if (event.phase.equals(TickEvent.Phase.START)) {
+        if (event.phase.equals(TickEvent.Phase.START) && event.side.isServer()) {
             MinecraftServer server = event.world.getServer();
-            GameRules gameRules = server.getGameRules();
-            if((System.currentTimeMillis() - time) >= 30000L && gameRules.getRule(GameRules.RULE_WEATHER_CYCLE).get()) {
-                time = System.currentTimeMillis();
-                gameRules.getRule(GameRules.RULE_WEATHER_CYCLE).set(false, server);
+            if(server != null) {
+                GameRules gameRules = server.getGameRules();
+                if ((System.currentTimeMillis() - time) >= 30000L && gameRules.getRule(GameRules.RULE_WEATHER_CYCLE).get()) {
+                    time = System.currentTimeMillis();
+                    gameRules.getRule(GameRules.RULE_WEATHER_CYCLE).set(false, server);
+                }
             }
         }
     }
